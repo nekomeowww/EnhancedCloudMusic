9649b76714b73018aac01264d4611120ea669524
Ayaka Neko <neko@ayaka.moe>
2017-10-05 18:53:06 +0800

    WebAPI Integration Init

---
+++ CloudMusicHelper/WebAPI.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace CloudMusicHelper
+{
+    class WebAPI
+    {
+        //Doc for webapi
+        //https://github.com/u3u/NeteaseCloudMusicApi
+
+        //Lyric
+        //http://music.163.com/api/song/lyric
+        //get('http://y.dskui.com/api/music/lyric?id=29775505').then(json => console.info(json));
+
+        //Playlist
+        //http://music.163.com/api/playlist/detail
+        //get('http://y.dskui.com/api/music/playlist?id=374755836').then(json => console.info(json));
+    }
+}
